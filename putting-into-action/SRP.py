7fa321fac78f101c44edd699008762b5f7df5705
greatbrain <erickfania@gmail.com>
2024-10-07 16:31:24 -0400

    .

---
+++ putting-into-action/SRP.py
@@ -0,0 +1,21 @@
+''' 
+Exercise: 
+We have an employee management system in a company. 
+Currently, a class called EmployeeManager handles new employee registration, report generation, 
+and salary calculation. 
+
+Refactor the design so that each of these responsibilities are in separate classes (for example, 
+EmployeeRegister, SalaryCalculator, and ReportGenerator). Each class should have a single responsibility.
+'''
+
+class EmployeeManager:
+    def __init__(self, emp_name:str, emp_last_name:str, emp_identification:str):
+        self.emp_name:str = emp_name
+        self.emp_last_name:str = emp_last_name
+        self.emp_identification:str = emp_identification
+
+        self.employees:dict = dict()
+
+
+    def add_employee(self):
+        pass
