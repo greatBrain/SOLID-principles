cbcf4b6aae414052bb591b6cd60657dafc60a048
greatbrain <erickfania@gmail.com>
2024-10-08 07:46:29 -0400

    Single responsability

--- putting-into-action/SRP.py
+++ putting-into-action/SRP.py
@@ -8,6 +8,9 @@ Refactor the design so that each of these responsibilities are in separate class
 EmployeeRegister, SalaryCalculator, and ReportGenerator). Each class should have a single responsibility.
 '''
 
+
+# Wrong code:
+
 class EmployeeManager:
     def __init__(self, emp_name:str, emp_last_name:str, emp_identification:str):
         self.emp_name:str = emp_name
@@ -17,6 +20,33 @@ class EmployeeManager:
         self.employees:dict = dict()
 
 
-    def add_employee(self, employee:dict):
-        if employee.name not in self.employees:
-           self.employees.update({"":""})  
+    def add_employee(self, employee:dict) -> bool:
+        if self.emp_name not in self.employees:
+           self.employees.update({"name":self.emp_name})  
+
+        return True
+
+    def get_employees(self) -> dict:
+        return self.employees
+
+
+    def get_employee_salary(self) -> float:
+        return 
+
+
+    ''' etc, etc....'''
+
+
+
+# Good code: we separate into classes and give just a responsability for each class.
+class Database:      
+      # Logic 
+
+class EmployeeRegister:
+      # Logic 
+
+class SalaryManager:
+      # Logic 
+
+class ReportManager:
+      # Logic
